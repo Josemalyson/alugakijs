6a801381b1c266e5db2ff9985118c1152a76a0fa
josemalyson <josemalyson@gmail.com>
2016-09-23 17:39:08 -0300

    ajustando css

---
+++ src/app/cliente/novo-cliente/novo-cliente.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { NovoClienteComponent } from './novo-cliente.component';
+
+describe('Component: NovoCliente', () => {
+  it('should create an instance', () => {
+    let component = new NovoClienteComponent();
+    expect(component).toBeTruthy();
+  });
+});
